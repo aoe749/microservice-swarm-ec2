97a69b23abd0c38da0f4434d26ad119e9726bfc2
aoe749 <viktormuradyan@gmail.com>
2021-11-10 16:02:17 +0400

    Update undeploy_swarm.sh

--- scripts/undeploy_swarm.sh
+++ scripts/undeploy_swarm.sh
@@ -12,6 +12,7 @@ export swarm_master=$(aws ec2 describe-instances \
   --filters Name='tag:Name,Values=Swarm-master' \
   --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
 echo "Swarm-master public ip: ${swarm_master}"
+ssh -oStrictHostKeyChecking=no -T -i ${pr_key} ec2-user@${swarm_master} docker swarm leave -f
 
 # export swarm_master_private_ip=$(aws ec2 describe-instances \
 #   --filters Name='tag:Name,Values=Swarm-master' \
@@ -19,29 +20,29 @@ echo "Swarm-master public ip: ${swarm_master}"
 # echo "Swarm-master private ip: ${swarm_master_private_ip}"
 
 # Swarm manager's IPs
-export swarm_manager_1=$(aws ec2 describe-instances \
-  --filters Name='tag:Name,Values=Swarm-manager-1' \
-  --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
-echo "Swarm-manager-1 public ip: ${swarm_manager_1}"
-
-export swarm_manager_2=$(aws ec2 describe-instances \
-  --filters Name='tag:Name,Values=Swarm-manager-2' \
-  --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
-echo "Swarm-manager-2 public ip: ${swarm_manager_2}"
-
-## Workers
-export worker_1=$(aws ec2 describe-instances \
-  --filters Name='tag:Name,Values=worker-1' \
-  --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
-echo "Worker1 public ip: ${worker_1}"
-export worker_2=$(aws ec2 describe-instances \
-  --filters Name='tag:Name,Values=worker-2' \
-  --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
-echo "Worker2 public ip: ${worker_2}"
-export worker_3=$(aws ec2 describe-instances \
-  --filters Name='tag:Name,Values=worker-3' \
-  --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
-echo "Worker3 public ip: ${worker_3}"
+# export swarm_manager_1=$(aws ec2 describe-instances \
+#   --filters Name='tag:Name,Values=Swarm-manager-1' \
+#   --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
+# echo "Swarm-manager-1 public ip: ${swarm_manager_1}"
+#
+# export swarm_manager_2=$(aws ec2 describe-instances \
+#   --filters Name='tag:Name,Values=Swarm-manager-2' \
+#   --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
+# echo "Swarm-manager-2 public ip: ${swarm_manager_2}"
+#
+# ## Workers
+# export worker_1=$(aws ec2 describe-instances \
+#   --filters Name='tag:Name,Values=worker-1' \
+#   --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
+# echo "Worker1 public ip: ${worker_1}"
+# export worker_2=$(aws ec2 describe-instances \
+#   --filters Name='tag:Name,Values=worker-2' \
+#   --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
+# echo "Worker2 public ip: ${worker_2}"
+# export worker_3=$(aws ec2 describe-instances \
+#   --filters Name='tag:Name,Values=worker-3' \
+#   --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
+# echo "Worker3 public ip: ${worker_3}"
 # export worker_4=$(aws ec2 describe-instances \
 #   --filters Name='tag:Name,Values=worker-4' \
 #   --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
@@ -60,72 +61,13 @@ echo "Worker3 public ip: ${worker_3}"
 #   --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
 # echo "Worker7 public ip: ${worker_7}"
 
-############################################################
-### Create SWARM, get join token and pass
-############################################################
 
-
-ssh -oStrictHostKeyChecking=no -T -i ${pr_key} ec2-user@${swarm_manager_1} docker swarm leave -f
-
-ssh -oStrictHostKeyChecking=no -T -i ${pr_key} ec2-user@${swarm_manager_2} docker swarm leave -f
-
-ssh -oStrictHostKeyChecking=no -T -i ${pr_key} ec2-user@${worker_1} docker swarm leave -f
-
-ssh -oStrictHostKeyChecking=no -T -i ${pr_key} ec2-user@${worker_2} docker swarm leave -f
-
-ssh -oStrictHostKeyChecking=no -T -i ${pr_key} ec2-user@${worker_3} docker swarm leave -f
-
-ssh -oStrictHostKeyChecking=no -T -i ${pr_key} ec2-user@${swarm_master} docker swarm leave -f
-# ssh -oStrictHostKeyChecking=no -T \
-#   -i ~/.ssh/jenkins-Frankfurt.pem ec2-user@${worker_4} << EOSSH
-# sudo hostnamectl set-hostname worker-4
-# docker swarm leave -f
-# ${join_wrk}
-# EOSSH
-# echo "4th ready"
-# echo "#################"
+# ssh -oStrictHostKeyChecking=no -T -i ${pr_key} ec2-user@${swarm_manager_1} docker swarm leave -f
 #
-# ssh -oStrictHostKeyChecking=no -T \
-#   -i ~/.ssh/jenkins-Frankfurt.pem ec2-user@${worker_5} << EOSSH
-# sudo hostnamectl set-hostname worker-5
-# docker swarm leave -f
-# ${join_wrk}
-# EOSSH
-# echo "5th ready"
-# echo "#################"
+# ssh -oStrictHostKeyChecking=no -T -i ${pr_key} ec2-user@${swarm_manager_2} docker swarm leave -f
 #
-# ssh -oStrictHostKeyChecking=no -T \
-#   -i ~/.ssh/jenkins-Frankfurt.pem ec2-user@${worker_6} << EOSSH
-# sudo hostnamectl set-hostname worker-6
-# docker swarm leave -f
-# ${join_wrk}
-# EOSSH
-# echo "6th ready"
+# ssh -oStrictHostKeyChecking=no -T -i ${pr_key} ec2-user@${worker_1} docker swarm leave -f
 #
-# echo "#################"
-
-# ssh -oStrictHostKeyChecking=no -T \
-#   -i ~/.ssh/jenkins-Frankfurt.pem ec2-user@${worker_7} << EOSSH
-# sudo hostnamectl set-hostname worker-7,
-# docker swarm leave -f
-# ${join_wrk}
-# EOSSH
-# echo "7th ready"
-
-
-#############################################################
-### Fetching consul configs and send to SWARM
-#############################################################
-# aws s3 cp
+# ssh -oStrictHostKeyChecking=no -T -i ${pr_key} ec2-user@${worker_2} docker swarm leave -f
 #
-# scp -oStrictHostKeyChecking=no -T -i ${pr_key} consul/consul/config.json.s ec2-user@${swarm_master}:consul/config/config.json
-# scp -oStrictHostKeyChecking=no -T -i ${pr_key} compose/docker-full.yml ec2-user@${swarm_master}:/home/ec2-user
-# scp -oStrictHostKeyChecking=no -T -i ${pr_key} consul/config.json.s1 ec2-user@${swarm_manager_1}:consul/config/config.json
-# scp -oStrictHostKeyChecking=no -T -i ${pr_key} consul/config.json.s2 ec2-user@${swarm_manager_2}:consul/config/config.json
-
-# scp -oStrictHostKeyChecking=no -T -i ~/.ssh/jenkins-Frankfurt.pem scripts/consul/config.json.a1 ec2-user@${worker_1}:consul/config/config.json
-# scp -oStrictHostKeyChecking=no -T -i ~/.ssh/jenkins-Frankfurt.pem scripts/consul/config.json.a2 ec2-user@${worker_2}:consul/config/config.json
-# scp -oStrictHostKeyChecking=no -T -i ~/.ssh/jenkins-Frankfurt.pem scripts/consul/config.json.a3 ec2-user@${worker_3}:consul/config/config.json
-# scp -oStrictHostKeyChecking=no -T -i ~/.ssh/jenkins-Frankfurt.pem scripts/consul/config.json.a4 ec2-user@${worker_4}:consul/config/config.json
-# scp -oStrictHostKeyChecking=no -T -i ~/.ssh/jenkins-Frankfurt.pem scripts/consul/config.json.a5 ec2-user@${worker_5}:consul/config/config.json
-# scp -oStrictHostKeyChecking=no -T -i ~/.ssh/jenkins-Frankfurt.pem scripts/consul/config.json.a6 ec2-user@${worker_6}:consul/config/config.json./de
+# ssh -oStrictHostKeyChecking=no -T -i ${pr_key} ec2-user@${worker_3} docker swarm leave -f
